4c36177cc2fe31c17c63f90bf4f146f624584c34
belal480 <amibelal20174@gmail.com>
2024-07-16 09:50:49 +0600

    adding js

--- js/script.js
+++ js/script.js
@@ -1,18 +1,24 @@
-document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener('DOMContentLoaded', function() {
     const navLinks = document.querySelectorAll('.nav-link');
-
+    
+    // Function to handle the active class
+    function handleActiveClass(event) {
+        // Remove 'active' class from all links
+        navLinks.forEach(nav => nav.classList.remove('active'));
+        
+        // Add 'active' class to the clicked link
+        event.target.classList.add('active');
+    }
+    
+    // Attach click event listener to each nav link
     navLinks.forEach(link => {
-        link.addEventListener('click', function () {
-            navLinks.forEach(nav => nav.classList.remove('active'));
-            this.classList.add('active');
-        });
+        link.addEventListener('click', handleActiveClass);
     });
 });
 
-
     document.addEventListener("DOMContentLoaded", function() {
         window.addEventListener('scroll', function() {
-            if (window.scrollY > 50) { // Adjust the scroll value as needed
+            if (window.scrollY > 50) { 
                 document.querySelector('.navigation-wrap').classList.add('scrolled');
             } else {
                 document.querySelector('.navigation-wrap').classList.remove('scrolled');
